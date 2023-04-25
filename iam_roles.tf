dd51e565e07fc24cff82e05f97c10f56f56f165f
tomer <tomerschwartz2411@gmail.com>
2023-04-25 16:27:15 +0300

    add eks cluster creation via TF with IAM role, vpc and subnets

---
+++ iam_roles.tf
@@ -0,0 +1,21 @@
+resource "aws_iam_role" "eks_cluster_role" {
+  name = "eks-cluster-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cluster_role_attachment" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks_cluster_role.name
+}
